9d31767eebc4e5f73c9c6fd2e6ff34999a444155
Gonzalo Ortiz <gonzaortiz1506@gmail.com>
2022-12-12 14:53:39 -0300

    card para cuestionarios

--- src/views/QuizesView.vue
+++ src/views/QuizesView.vue
@@ -5,18 +5,20 @@
     </div>
     <div class="d-flex align-items-center">
       <div col>
-      <h6 class="h6-custom mt-2">Buscar: </h6>
+        <h6 class="h6-custom mt-2">Buscar: </h6>
+      </div>
+      <div col>
+        <input class="form-control ms-3" type="text" />
+      </div>
     </div>
-    <div col>
-      <input class="form-control ms-3"  type="text"/>
-    </div>
-    </div>
-    <div class="row">
+    <div class="row" v-for="item in notes" :key="item.id">
       <div class="col-sm-3 " v-for="item in notes" :key="item.id">
-        <div class="card setting" :style="{ backgroundColor: item.backgroundColor }">
-          <div class="card-body">
-            <h5 class="card-text">{{ item.text }}</h5>
-            <p class="card-title">{{ item.date.toLocaleDateString("es-AR") }}</p>
+        <div class="card card-image-custom">
+          <img class="card-img" :src="item.image">
+          <div class="card-body ">
+            <h5 class="card-title">{{item.title}}</h5>
+            <p class="card-text">{{item.text}}</p>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
           </div>
         </div>
       </div>
@@ -31,17 +33,30 @@
 import { ref } from "vue";
 
 const notes = ref(<any>[{
-    id: Math.floor(Math.random() * 1000000),
-    text: 'some value',
-    date: new Date()
-  }])
+  id: Math.floor(Math.random() * 1000000),
+  title: 'Matemática',
+  text: 'Prueba tus habilidades matemáticas.',
+  image: 'https://phantom-marca.unidadeditorial.es/1c4cbdc04587cda2e9268e878cd47168/resize/660/f/webp/assets/multimedia/imagenes/2022/12/10/16706837224997.jpg',
+}])
 
 
 
 
 </script>
 <style scoped>
-.h6-custom{
+.h6-custom {
   margin-left: 12px;
+}
+
+.custom-img {
+  margin: 0;
+}
+
+.card-image-custom {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 10px 10px 10px 10px;
+  padding: 0;
+  max-width: 98%;
+  font-family: arial;
 }
 </style>
