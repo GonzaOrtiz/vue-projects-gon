2dfcd942b9bb7151dd5b55bc0618b310c9f5a9df
Gonzalo Ortiz <gonzaortiz1506@gmail.com>
2022-12-12 21:25:34 -0300

    avances en quizes-detail

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="bg- backcolor  text-center text-lg-start">
+        <div class="text-center p-3 textcolor" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2023 Gonza Ortiz
+            <a class="text textcolor">vue-projects</a>
+        </div>
+    </footer>
+</template>
--- src/views/Quizes/QuizesDetail.vue
+++ src/views/Quizes/QuizesDetail.vue
@@ -1,9 +1,38 @@
 <template>
     <div class="container-fluid setting-principal-card">
         <div class="d-flex justify-content-between align-items-center">
-            <h3 class="h3-custom mb-2">Cuestionarios</h3>
-            <h1>Detail</h1>
-            <h2>{{ quiz }}</h2>
+            <h3 class="h3-custom mb-2">Cuestionario: {{ quiz?.title }}</h3>
+        </div>
+        <div class="d-flex justify-content-center ">
+            <h5 class="h6-custom mt-2">Pregunta 1/3 </h5>
+        </div>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+                        aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminateDisabled" disabled>
+            <label class="form-check-label" for="flexCheckIndeterminateDisabled">
+                Disabled indeterminate checkbox
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
+            <label class="form-check-label" for="flexCheckDisabled">
+                Disabled checkbox
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckCheckedDisabled" checked disabled>
+            <label class="form-check-label" for="flexCheckCheckedDisabled">
+                Disabled checked checkbox
+            </label>
         </div>
     </div>
 </template>
