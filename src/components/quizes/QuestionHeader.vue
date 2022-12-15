c0dccaa2bbf2d34b085fc64e78c4224ce860edd3
Gonzalo Ortiz <gonzaortiz1506@gmail.com>
2022-12-15 15:06:35 -0300

    avances en cuestionario

---
+++ src/components/quizes/QuestionHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="d-flex justify-content-between align-items-center">
+        <h3 class="h3-custom mb-2">Cuestionario: {{ title }}</h3>
+    </div>
+    <div class="d-flex justify-content-center ">
+        <h5 class="h6-custom mt-2">Pregunta {{ counter + 1 }}/3 </h5>
+    </div>
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="progress mb-4">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+                    aria-valuenow="66" aria-valuemin="0" aria-valuemax="100" :style="{ width: bar }">{{ bar }}</div>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script lang="ts" setup>
+defineProps({
+    title: String,
+    counter: Number,
+    bar: String
+})
+</script>
--- src/views/Quizes/QuizesDetail.vue
+++ src/views/Quizes/QuizesDetail.vue
@@ -1,33 +1,16 @@
 <template>
     <div class="container-fluid setting-principal-card">
-        <div class="d-flex justify-content-between align-items-center">
-            <h3 class="h3-custom mb-2">Cuestionario: {{ quiz?.title }}</h3>
-        </div>
-        <div class="d-flex justify-content-center ">
-            <h5 class="h6-custom mt-2">Pregunta {{counter+1}}/3 </h5>
-        </div>
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <div class="progress mb-4">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
-                        aria-valuenow="66" aria-valuemin="0" aria-valuemax="100" :style="{width: bar}">{{bar}}</div>
-                </div>
-            </div>
-        </div>
-
+        <QuestionHeaderVue :title="quiz?.title" :counter="counter" :bar="bar"></QuestionHeaderVue>
         <div class="setting-principal-card">
             <div class="container">
                 <h5 class="h6-custom mt-2 mb-3 ms-4">{{quiz?.questions[counter].id}} - {{ quiz?.questions[counter].text }} </h5>
                 <div class="container">
                     <div class="row">
                         <div class="col1" v-for="option in quiz?.questions[counter].options" :key="option.id">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value=""
-                                    id="flexCheckIndeterminateDisabled">
-                                <label class="form-check-label" for="flexCheckIndeterminateDisabled">
-                                    {{ option.label }} - {{ option.text }}
-                                </label>
-                            </div>
+                            <input class="form-check-input" type="radio" name="flexRadioDisabled" id="flexRadioDisabled">
+                            <label class="form-check-label ms-3" for="flexRadioDisabled">
+                                {{ option.label }} - {{ option.text }}
+                            </label>
                         </div>
                     </div>
                 </div>
@@ -46,11 +29,13 @@
 import { useRoute } from 'vue-router';
 import { ref } from 'vue';
 import quizes from "../../data/quizes.json"
+import QuestionHeaderVue from '@/components/quizes/QuestionHeader.vue';
 
 const route = useRoute();
 const quiz = quizes.find(q => q.id === parseInt(route.params.id.toString()));
 const counter = ref(0);
 const bar = ref("0%")
+const active = ref("checked")
 
 const nextQuestion = () =>{
     if(counter.value ===0){
